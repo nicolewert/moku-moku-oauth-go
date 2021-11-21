0f7d24b3478c02a9147b51599d27a5c318e68a77
Nicole Wert <nicalexxandra@gmail.com>
2021-11-21 14:26:07 +0900

    #7 feat: add rest errors

---
+++ oauth/errors/errors.go
@@ -0,0 +1,43 @@
+package errors
+
+import (
+	"net/http"
+)
+
+type RestErr struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+	Error   string `json:"error"`
+}
+
+func BadRequest(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+func NotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
+	}
+}
+
+func InternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
+	}
+}
+
+func UniqueConstraintViolation(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusUnprocessableEntity,
+		Error:   "unprocessable_entity",
+	}
+}
